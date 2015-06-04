c43ebf177a5de98dc79fc63d89cb5b1d6b5d918f
Michael Smith <michael@s0lder.com>
2015-06-04 03:46:57 -0400

    no message

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 
 void k_main(multiboot_info_t *mbd, unsigned int magic) {
     k_fbclear();
-    k_fbprint("Entering kernel main!");
+    k_fbprint("Entering kernel main!\n");
+    k_fbprint("Using early_fb driver\n");
     if(magic != MULTIBOOT_BOOTLOADER_MAGIC) {
         k_fbputstring(0, 1,
                 "Error: Not loaded by multiboot bootloader. Will now exit.",
